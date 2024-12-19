a6c7ef4c421d11e7bf96ecb727b19f90c2006cfd
seongyon98 <seongyon98@gmail.com>
2024-12-19 10:19:38 +0900

    api 조회 페이지 추가(아직 테스트는 못함)

--- labelling/labelling/settings.py
+++ labelling/labelling/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "upload",
+    "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
@@ -123,9 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
--- labelling/upload/urls.py
+++ labelling/upload/urls.py
@@ -1,5 +1,20 @@
 from django.urls import path
 from .views import UploadFileAPIView, GetResultAPIView, upload_page
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger UI를 위한 설정
+schema_view = get_schema_view(
+    openapi.Info(
+        title="자동 라벨링 시스템 API",
+        default_version="v1",
+        description="자동 라벨링 시스템의 API 문서입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("upload/", upload_page, name="upload_page"),  # HTML 페이지 렌더링
@@ -9,4 +24,7 @@ urlpatterns = [
     path(
         "api/results/<str:file_name>/", GetResultAPIView.as_view(), name="get_result"
     ),  # 결과 조회 API
+    path(
+        "swagger/", schema_view.as_view(), name="swagger"
+    ),  # Swagger UI 엔드포인트 추가
 ]
